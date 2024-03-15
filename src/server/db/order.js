0077fd559865a585016981b2f2539f8744018aa5
Ian Balderston <ianmb@vt.edu>
2024-03-15 11:58:37 -0400

    filled out orders db functions

---
+++ src/server/db/order.js
@@ -0,0 +1,56 @@
+const db = require('./client')
+
+// -create neworder, -getorder function, 
+// updateorders (patch: shipping-address, status), 
+
+const createOrder = async ({ userId }) => {
+    try{
+      const { rows: [ order ] } = await db.query(`
+        INSERT INTO orders(userId, status)
+        VALUES ($1, $2)
+        RETURNING *`, [ userId, true ]);
+        return order;
+    } catch (err) {
+        throw err;
+      }
+    }
+
+const getOrder = async ({ orderId }) => {
+    try{
+      const { rows } = await db.query(`
+        SELECT * FROM orders 
+        WHERE id=$1 
+        `, [ orderId ]);
+        return rows;
+    } catch (err) {
+        throw err;
+      }
+    }
+
+const updateAddress = async (orderId, address) => {
+    try{
+        const { rows } = await db.query(`
+        UPDATE orders
+        SET address=$2
+        WHERE  id= $1
+        RETURNING *
+        `, [ orderId, address ]);
+        return rows;
+    } catch (err) {
+        throw err;
+        }
+    }
+
+const updateStatus = async (orderId, status) => {
+    try{
+        const { rows } = await db.query(`
+        UPDATE orders
+        SET status=$2
+        WHERE id= $1
+        RETURNING *
+        `, [ orderId, status ]);
+        return rows;
+    } catch (err) {
+        throw err;
+        }
+    }
