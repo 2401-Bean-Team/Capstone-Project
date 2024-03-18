5aa6d49a12329d01308665c8a6ee37459446b0ad
EthanYoshika <eyoshika780@gmail.com>
2024-03-18 09:35:53 -0700

    allproducts

--- src/client/components/AllProducts.jsx
+++ src/client/components/AllProducts.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
 
 import axios from "axios"
+import { Link } from 'react-router-dom'
 
 
 export default function AllProducts() {
@@ -12,9 +12,16 @@ export default function AllProducts() {
             const { data } = await axios.get('/api/products')
 
             setCoffee(data)
-        } 
+        }
         fetchCoffee()
     }, [])
+    console.log( coffee )
 
-    return <h1>allproducts</h1>
+    return <div className="allCoffee">
+        <h1>Coffee:</h1>
+        {coffee.map(c => <div key = {c.id} className="coffee">
+            <Link to={`/${c.name}`}><img src={c.image} alt={c.name} />  <h1>{c.name}</h1><h2>{c.price}</h2><h2>{c.roast}</h2></Link>
+            </div>)}
+
+    </div>
 }
