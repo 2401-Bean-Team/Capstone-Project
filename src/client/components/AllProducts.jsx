1d1918245d495e57a4fc86e0de9770b623805553
Ian Balderston <ianmb@vt.edu>
2024-03-18 12:02:14 -0400

    fetchCoffee

--- src/client/components/AllProducts.jsx
+++ src/client/components/AllProducts.jsx
@@ -1,7 +1,20 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
+import axios from "axios"
+
 
 export default function AllProducts() {
+    const [coffee, setCoffee] = useState([]);
+
+    useEffect(() => {
+        async function fetchCoffee() {
+            const { data } = await axios.get('/api/products')
+
+            setCoffee(data)
+        } 
+        fetchCoffee()
+    }, [])
+
     return <h1>allproducts</h1>
 }
