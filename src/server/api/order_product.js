a0b978106abbb6af3acd09373e55b79bb8e7ed3d
MarcusDarnell <mdarnell12894@gmail.com>
2024-03-19 11:10:48 -0700

    Start-of-cart

--- src/server/api/order_product.js
+++ src/server/api/order_product.js
@@ -1,3 +1,39 @@
 const express = require('express');
-const coffeeRouter = express.Router();
+const cartRouter = express.Router();
+const { addProduct, deleteCartSingle, deleteCart, updateQuantity } = require('../db/order_product')
 
+cartRouter.get('/:productId', async(req, res, next) => {
+  try {
+    const cart = await addProduct();
+    res.send(cart)
+  } catch(err) {
+    next(err);
+  }
+});
+
+cartRouter.delete( , async(req, res, next) => {
+    try {
+      const cart = await deleteCartSingle();
+    } catch(err) {
+      next(err);
+    }
+  });
+
+  cartRouter.delete( , async(req, res, next) => {
+    try {
+      const cart = await deleteCart();
+    } catch(err) {
+      next(err);
+    }
+  });
+
+  cartRouter.get( , async(req, res, next) => {
+    try {
+      const cart = await updateQuantity
+    } catch(err) {
+      next(err);
+    }
+  });
+
+
+  // do we need a check out?
