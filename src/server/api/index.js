1f898c68282301b3429c6a0d02eef2556397805a
Ian Balderston <ianmb@vt.edu>
2024-03-19 11:44:02 -0400

    login request working

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -2,22 +2,37 @@ const express = require('express');
 const apiRouter = express.Router();
 const jwt = require('jsonwebtoken');
 
+const { getUser } = require( '../db/users')
+const { JWT_SECRET } = process.env;
+
 const volleyball = require('volleyball')
 apiRouter.use(volleyball)
 
 // TO BE COMPLETED - set `req.user` if possible, using token sent in the request header
 apiRouter.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
   const auth = req.header('Authorization');
 
   if (!auth) {
     next();
   }
-  else if (auth.startsWith('REPLACE_ME')) {
+  else if (auth.startsWith(prefix)) {
     // TODO - Get JUST the token out of 'auth'
-    const token = 'REPLACE_ME';
+    const token = auth.slice(prefix.length);
 
     try {
-      const parsedToken = 'REPLACE_ME';
+      const parsedToken = jwt.verify(token, JWT_SECRET);
+      const { id } = parsedToken
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      } else {
+        next({
+          name: 'AuthorizationHeaderError',
+          message: 'Authorization token malformed',
+        });
+      }
       // TODO - Call 'jwt.verify()' to see if the token is valid. If it is, use it to get the user's 'id'. Look up the user with their 'id' and set 'req.user'
 
     } catch (error) {
