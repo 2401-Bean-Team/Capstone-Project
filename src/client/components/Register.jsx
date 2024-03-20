990463f336b8edbef3f195c98487f81da0689313
Kyle Beaven <kbeaven17@gmail.com>
2024-03-20 12:15:51 -0600

    register

--- src/client/components/Register.jsx
+++ src/client/components/Register.jsx
@@ -1,3 +1,96 @@
-export default function Register() {
-    return <h1>Register</h1>
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const register = async() => {
+    try {
+        const response = await fetch('/api/users/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            }, 
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        });
+        const result = await response.json();
+        setMessage(result.message);
+        if(!response.ok) {
+          throw(result)
+        }
+        setName('');
+        setEmail('');
+        setPassword('');
+    } catch (err) {
+        console.error(`${err.name}: ${err.message}`);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    register();
+  };
+
+
+  return (
+    <div>
+      
+      <form onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="text"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      <p>{message}</p>
+    </div>
+  );
 }
+
+export default Register;
+
+/*export default function Register() {
+    return <h1>Register</h1>
+}*/
