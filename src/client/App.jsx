8c3615c5f402c47f983fc62dd9f15a4f2ba95496
Ian Balderston <ianmb@vt.edu>
2024-03-21 14:23:58 -0400

    merge

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,7 +7,6 @@ import Register from './components/Register';
 import { Route, Routes, NavLink } from 'react-router-dom'
 import NavBar from './components/NavBar';
 import AdminLogin from './components/AdminLogin';
-import ShoppingCart from './components/Cart';
 
 
 function App() {
@@ -31,43 +30,6 @@ function App() {
       <Route path='/register' element={<Register setToken={setToken} />} />
       <Route path='/cart' element={<Register token={token} />} />
       <Route path='/admin' element={<AdminLogin setToken={setToken} name={name} setName={setName} password={password} setPassword={setPassword} />} />
-    </Routes>
-</div>
-  );
-}
-
-export default App;
-import { useState } from 'react';
-import Login from './components/Login';
-import AllProducts from './components/AllProducts';
-import SingleProduct from './components/SingleProduct';
-import Account from './components/Account';
-import Register from './components/Register';
-import { Route, Routes, NavLink } from 'react-router-dom'
-import NavBar from './components/NavBar';
-import ShoppingCart from './components/Cart';
-
-
-function App() {
-  const [token, setToken] = useState(null)
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  
-
-  return (
-< div className = 'App'>
-<header>
-        <h1>Coding Coffee</h1>
-        <NavBar className="navagation" />
-      </header>
-    <Routes>
-
-      <Route path='/' element={<AllProducts />} />
-      <Route path='/coffee/:productId' element={<SingleProduct />} />
-      <Route path='/login' element={<Login setToken={setToken} email={email} setEmail={setEmail} password={password} setPassword={setPassword} />} />
-      <Route path='/account' element={<Account token={token} setToken={setToken} email={email} setEmail={setEmail} password={password} setPassword={setPassword} />} />
-      <Route path='/register' element={<Register setToken={setToken} />} />
-      <Route path='/cart' element={<ShoppingCart token={token} email={email} />} />
     </Routes>
 </div>
   );
