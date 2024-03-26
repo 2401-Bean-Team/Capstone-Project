c4c067501c563332048ee4f732440aa4ac97dea6
Kyle Beaven <kbeaven17@gmail.com>
2024-03-26 11:40:05 -0600

    clean up

--- src/client/components/SingleProduct.jsx
+++ src/client/components/SingleProduct.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom";
-import '../singleitem.css';
+
 
 
 import axios from 'axios'
